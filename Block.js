6844ac8e6e30458f6c7c2b61b8d8d2feb1182843
JejuCjm <cjm6794@gmail.com>
2023-03-14 15:06:41 +0900

    no message

--- Block.js
+++ Block.js
@@ -354,7 +354,7 @@ const LibraryCreator = {
       if (typeof useWebGL == "undefined") {
         updateCategory(category)
         // 아이콘 적용
-        $('head').append(`<style>#entryCategory${category}{background-image:url(https://raw.githack.com/JEJUCE/EntryBlock/main/logo.png);background-repeat:no-repeat;margin-bottom:1px;background-position-y: 10px;background-size: 20px;}.entrySelectedCategory#entryCategory${category}{background-image:url(https://raw.githack.com/JEJUCE/EntryBlock/main/logo.png);background-color:#000000;border-color:#000000;color:#fff}</style>`)
+        $('head').append(`<style>#entryCategory${category}{background-image:url(https://raw.githack.com/JEJUCE/EntryBlock/main/logo.svg);background-repeat:no-repeat;margin-bottom:1px;backgrfpound-position-y: 10px;background-size: 20px;}.entrySelectedCategory#entryCategory${category}{background-image:url(https://raw.githack.com/JEJUCE/EntryBlock/main/logo.svg);background-color:#000000;border-color:#000000;color:#fff}</style>`)
         // 카테고리 이름 적용
         $(`#entryCategory${category}`).append(text)
       }
@@ -736,4 +736,4 @@ return data; // 반환
 ]
   alert('로딩완료')
 document.title = "Entry_Strong_Block";
-LibraryCreator.start(blocks, 'API', '강력크')
+LibraryCreator.start(blocks, 'API', 'Test')
